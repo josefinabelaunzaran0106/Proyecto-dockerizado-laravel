07e1572b928fe003b3bd2b0ae5b683ef0bb0ac99
Josefina Belaunzaran <josefina.belaunzaran@rec.unicen.edu.ar>
2026-09-22 12:51:44 -0300

    modifico routes web

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
-Route::get('/hola', function () {
-    return 'Hola Josefina 👋';
-});
+
+Route::get('/login', [LoginController::class, 'showLogin'])
+    ->name('login');
+
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::post('/logout', [LoginController::class, 'logout'])
+    ->middleware('auth');
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
